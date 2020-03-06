5e47b6288193099a24d8e22911d8edca12021e8f
Your Name <you@example.com>
2020-03-06 15:33:03 +0000

    part 2, mac only

--- blescan.py
+++ blescan.py
@@ -32,32 +32,22 @@ def get_events(sock, loop_count=10000):
     bluez.hci_filter_all_events(flt)
     bluez.hci_filter_set_ptype(flt, bluez.HCI_EVENT_PKT)
     sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, flt)
-    blacklist=[]
     for i in range(0, loop_count):  # pylint: disable=unused-variable
         pkt = sock.recv(255)
-        print(find_mac(pkt,blacklist))
+        print(find_mac(pkt))
     sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, old_filter)
 
 
-def find_mac(pkt,blacklist):
+def packed_bdaddr_to_string(bdaddr_packed):
+    #  iBeacon packets have the mac byte-reversed, reverse with bdaddr_packed[::-1]  # noqa
+    #  b'ID\x8b\xea&b' -> b'b&\xea\x8bDI'
+    #  decode to int -> (98, 38, 234, 139, 68, 73) , join by : as hex -> '62:26:ea:8b:44:49'  # noqa
+    return ':'.join('%02x' % i for i in struct.unpack("<BBBBBB", bdaddr_packed[::-1]))  # noqa
+
+
+def find_mac(pkt):
     ptype, event, plen = struct.unpack("BBB", pkt[:3])  # b'\x04>+' -> (4, 62, 40)  # pylint:disable=unused-variable  # noqa
     if event == 0x3e:  # 62 -> 0x3e -> HCI Event: LE Meta Event (0x3e) plen 44
         subevent, = struct.unpack("B", pkt[3:4])  # b'\x02' -> (2,)
         if subevent == 0x02:  # if 0x02 (2) -> all iBeacons use this
-            print('test')
-            if pkt not in blacklist:
-                print('Looking for: ed:72:01:03:01:02')
-                found=0
-                for i in range(0, len(pkt)):
-                    _hex = "%02x" % struct.unpack("<B", pkt[i:i+1])[0]
-                    if _hex == "ed" or _hex == '72' or _hex == '03':
-                        found+=1
-                    print("[%d:%d] %s" % (i, i+1, _hex))
-                if found > 2:
-                    import pdb
-                    pdb.set_trace()
-
-_socket = bluez.hci_open_dev(0)
-hci_le_set_scan_parameters(_socket)
-hci_enable_le_scan(_socket)
-get_events(_socket)
+            return packed_bdaddr_to_string(pkt[3:9])
