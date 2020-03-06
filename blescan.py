c50af9f1e8b44ff3cf4a7ef54a925d373c39b312
Your Name <you@example.com>
2020-03-06 15:40:26 +0000

    part 3, uuid

--- blescan.py
+++ blescan.py
@@ -34,7 +34,7 @@ def get_events(sock, loop_count=10000):
     sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, flt)
     for i in range(0, loop_count):  # pylint: disable=unused-variable
         pkt = sock.recv(255)
-        print(find_mac(pkt))
+        print(parse_packet(pkt))
     sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, old_filter)
 
 
@@ -44,10 +44,44 @@ def packed_bdaddr_to_string(bdaddr_packed):
     #  decode to int -> (98, 38, 234, 139, 68, 73) , join by : as hex -> '62:26:ea:8b:44:49'  # noqa
     return ':'.join('%02x' % i for i in struct.unpack("<BBBBBB", bdaddr_packed[::-1]))  # noqa
 
+def string_packet(pkt):
+    #  UUID is 16 Bytes
+    # b'\xfe\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    # so len() is 16
+    # loop over each byte, get it to hex, build up the string (uuid is 32 chars, 16bytes)  # noqa
+    _str = ""
+    for i in range(len(pkt)):  # 0-16 loop
+        _str += "%02x" % struct.unpack("B", pkt[i:i+1])[0]
+    return _str
 
-def find_mac(pkt):
+# https://github.com/atlefren/pytilt/blob/master/pytilt.py
+TILT_DEVICES = {
+    'a495bb30c5b14b44b5121370f02d74de': 'Black',
+    'a495bb60c5b14b44b5121370f02d74de': 'Blue',
+    'a495bb20c5b14b44b5121370f02d74de': 'Green',
+    'a495bb50c5b14b44b5121370f02d74de': 'Orange',
+    'a495bb80c5b14b44b5121370f02d74de': 'Pink',
+    'a495bb40c5b14b44b5121370f02d74de': 'Purple',
+    'a495bb10c5b14b44b5121370f02d74de': 'Red',
+    'a495bb70c5b14b44b5121370f02d74de': 'Yellow',
+    'a495bb90c5b14b44b5121370f02d74de': 'Test',
+}
+
+
+def parse_packet(pkt):
+    # http://www.havlena.net/wp-content/themes/striking/includes/timthumb.php?src=/wp-content/uploads/ibeacon-packet.png&w=600&zc=1
+    #pkt = b'   \x04>*                      \x02\x01x03\x01w\t  \xbc\xd0W\xef\x1e\x02\x01\x04\x1a\xffL\x00\x02\x15    \xa4\x95\xbb0\xc5\xb1KD\xb5\x12\x13p\xf0-t\xde  \x00B  \x03\xf7   \xc5\xa7'   # noqa
+    #       |                  |           |                   |                                                    |                                                |      |         |          |  # noqa
+    #       | preamble+header  |                         PDU                                                                                                                                     |  # noqa
+    #       |     3 bytes      |                        x bytes (plen)                                                                                                                           |  # noqa
+    #       |                  |           |    mac addr       |           unused                                   |          uuid                                  | major| minor   |   tx     |  # noqa
+    #       |                  |           |                   |                                                    |                                                | temp | gravity |          |  # noqa
     ptype, event, plen = struct.unpack("BBB", pkt[:3])  # b'\x04>+' -> (4, 62, 40)  # pylint:disable=unused-variable  # noqa
     if event == 0x3e:  # 62 -> 0x3e -> HCI Event: LE Meta Event (0x3e) plen 44
         subevent, = struct.unpack("B", pkt[3:4])  # b'\x02' -> (2,)
         if subevent == 0x02:  # if 0x02 (2) -> all iBeacons use this
-            return packed_bdaddr_to_string(pkt[3:9])
+            return {
+                'mac': packed_bdaddr_to_string(pkt[3:9]),  # mac   -> 6 bytes -> b'\x02\x01\x03\x01w\t'  # noqa
+                'uuid': string_packet(pkt[-22:-6]),        # uuid  -> 16bytes -> b'\xa4\x95\xbb0\xc5\xb1KD\xb5\x12\x13p\xf0-t\xde'  # noqa
+            }
+    return {}
